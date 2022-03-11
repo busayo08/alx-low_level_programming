b3cfa9701d9055f4cf327cc8ba7f44feed8bc728
Busayo <akinsolabusayo@gmail.com>
2022-03-11 09:27:30 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase with putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char ch;
+char new_line;
+new_line = '\n';
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+	putchar(ch);
+}
+putchar(new_line);
+return (0);
+}
+© 2022 GitHub, Inc.
