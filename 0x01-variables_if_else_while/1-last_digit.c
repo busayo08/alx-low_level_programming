e9d16d131c001d1060f457deeeb0cacd18b4cc00
Busayo <akinsolabusayo@gmail.com>
2022-03-19 07:36:56 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,20 +1,41 @@
+/*
+ * File: 1-last_digit.c
+ * Auth: Akinsola Busayo
+ */
+
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-/*
- * main - Prints alphabet in lowercase with putchar
+/**
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-char ch;
-char new_line;
-new_line = '\n';
+	int n;
 
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-	putchar(ch);
-}
-putchar(new_line);
-return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if ((n % 10) > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n",
+			n, n % 10);
+	}
+	else if ((n % 10) < 6 && (n % 10) != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+			n, n % 10);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n",
+			n, n % 10);
+	}
+
+	return (0);
+
 }
